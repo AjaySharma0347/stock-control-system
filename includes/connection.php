78b5bad8b21e111a24fefcc251164d87fc407ed5
unknown <ajaysharma98129@gmail.com>
2024-11-07 22:18:45 +0530

    frst upload

---
+++ includes/connection.php
@@ -0,0 +1,8 @@
+<?php
+ $db = mysqli_connect('localhost', 'root', '') or
+        die ('Unable to connect. Check your connection parameters.');
+        mysqli_select_db($db, 'scms' ) or die(mysqli_error($db));
+        
+?>
+
+
